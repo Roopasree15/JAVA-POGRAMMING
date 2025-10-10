c79ffd8aa47703c0ffb986fb0d5bf1933f37d2a2
Roopasree P <roopasreesaraswathi58@gmail.com>
2025-10-10 10:27:09 +0530

    Add files via upload

---
+++ PrintOddNumbers.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class PrintOddNumbers {
+    public static void main(String[] args) {
+        int limit = 20;
+
+        for (int i = 1; i <= limit; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
